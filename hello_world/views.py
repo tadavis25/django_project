3f28ddc037326f39e2c3224cad70acd3e6391ba9
Miss Terry Davis <tdavis_909@hotmail.com>
2026-07-21 15:36:01 +0100

    Added a POST method to hello world views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here
+
+
 def index(request):
-    return HttpResponse("Hello, World!")
+    if request.method == 'POST':
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
